d7b0ceba11435ade43f7a6c0f9dfefe681553e40
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-09-01 12:26:38 +0200

    Add get_args function

--- mkpl.py
+++ mkpl.py
@@ -22,8 +22,63 @@
 
 """mkpl module. This is command line utility for playlist creation."""
 
+# region imports
+import argparse
+
+# endregion
+
 # region globals
 FILE_FORMAT = {'mp1', 'mp2', 'mp3', 'mp4', 'aac', 'ogg', 'wav', 'wma',
                'avi', 'xvid', 'divx', 'mpeg', 'mpg', 'mov', 'wmv'}
 
+
+# endregion
+
+# region functions
+def get_args():
+    """Get command-line arguments"""
+
+    global FILE_FORMAT
+
+    parser = argparse.ArgumentParser(
+        description="Make music playlist",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        epilog='Playlist format is m3u'
+    )
+
+    parser.add_argument("playlist", help="Playlist file", type=str)
+    parser.add_argument("-d", "--directories", help="Directories that contains multimedia file",
+                        nargs=argparse.ONE_OR_MORE, default=['.'])
+    parser.add_argument("-e", "--exclude-dirs", help="Exclude directory paths", nargs=argparse.ONE_OR_MORE, default=[])
+    parser.add_argument("-i", "--include", help="Include other file format", nargs=argparse.ONE_OR_MORE,
+                        metavar='FORMAT')
+    parser.add_argument("-p", "--pattern", help="Regular expression inclusion pattern", default='.*')
+    parser.add_argument("-f", "--format", help="Select only a file format", type=str, choices=FILE_FORMAT)
+    parser.add_argument("-z", "--size", help="Start size in bytes", type=int, default=1, metavar='BYTES')
+    parser.add_argument("-m", "--max-tracks", help="Maximum number of tracks", type=int, default=None, metavar='NUMBER')
+    parser.add_argument("-r", "--recursive", help="Recursive search", action='store_true')
+    parser.add_argument("-a", "--absolute", help="Absolute file name", action='store_true')
+    parser.add_argument("-s", "--shuffle", help="Casual order", action='store_true')
+    parser.add_argument("-u", "--unique", help="The same files are not placed in the playlist", action='store_true')
+    parser.add_argument("-c", "--append", help="Continue playlist instead of override it", action='store_true')
+
+    args = parser.parse_args()
+
+    # Check extension of playlist file
+    if not args.playlist.endswith('.m3u'):
+        args.playlist += '.m3u'
+
+    # Open playlist file
+    mode = 'at' if args.append else 'wt'
+    args.playlist = open(args.playlist, mode=mode)
+
+    # Extend files format
+    if args.include:
+        FILE_FORMAT.update(set(args.include))
+
+    # Select only one format
+    if args.format:
+        FILE_FORMAT = {args.format.strip('*').strip('.')}
+
+    return args
 # endregion
