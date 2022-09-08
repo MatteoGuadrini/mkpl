9f883c2501b3de7c17873f385f277a5508c7fcff
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-09-08 11:15:57 +0200

    Fix command line script name

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         ],
     entry_points={
         'console_scripts': [
-            'dinosay = mkpl:main'
+            'mkpl = mkpl:main'
         ]
     },
     python_requires='>=3.5'
