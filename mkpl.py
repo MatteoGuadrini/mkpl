7dd921ea8882ef29e57a34a7bfc668baa262f159
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-09-17 10:44:40 +0200

    Fix help link argument

--- mkpl.py
+++ mkpl.py
@@ -66,7 +66,7 @@ def get_args():
     parser.add_argument("-t", "--title", help="Playlist title", default=None)
     parser.add_argument("-g", "--encoding", help="Text encoding", choices=('UTF-8', 'ASCII', 'UNICODE'), default=None)
     parser.add_argument("-I", "--image", help="Playlist image", default=None)
-    parser.add_argument("-l", "--link", help="Add remote playlist link", nargs=argparse.ONE_OR_MORE, default=[])
+    parser.add_argument("-l", "--link", help="Add remote file links", nargs=argparse.ONE_OR_MORE, default=[])
     parser.add_argument("-r", "--recursive", help="Recursive search", action='store_true')
     parser.add_argument("-a", "--absolute", help="Absolute file name", action='store_true')
     parser.add_argument("-s", "--shuffle", help="Casual order", action='store_true')
