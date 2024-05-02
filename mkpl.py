eac615e4e31a57c9cd4c57ce385dd7cc28a921a9
Matteo Guadrini <matteo.guadrini@hotmail.it>
2024-05-02 10:03:38 +0200

    chore: add order by size argument

--- mkpl.py
+++ mkpl.py
@@ -213,6 +213,12 @@ def get_args():
         help="Order playlist files by year",
         action="store_true",
     )
+    orderby_group.add_argument(
+        "-Z",
+        "--orderby-size",
+        help="Order playlist files by size",
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
